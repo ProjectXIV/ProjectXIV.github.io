9d52487e71c3573b8767dc8a5c4cf1940f746fd3
Llana <145599991+ProjectXIV@users.noreply.github.com>
2024-03-05 15:27:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,71 +7,69 @@ class App extends React.Component {
 
     renderContent() {
         const { activeTab } = this.state;
-    
-        switch (activeTab) {
-            case 'ProjectXIV':
-                return (
-                    <>
-                        <table className="layout-table">
-                            <tbody>
-                                <tr>
-                                    <td width='700px'>
-                                        <div className="video-frame">
-                                            <div className="video-responsive">
-                                                <iframe
-                                                    width="600"
-                                                    height="338"
-                                                    src="https://www.youtube.com/embed/kZVZuW0VA4k"
-                                                    title="Hello World"
-                                                    frameBorder="0" 
-                                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                                    allowFullScreen={true}>
-                                                </iframe>
-                                            </div>
+
+        if (activeTab === 'ProjectXIV') {
+            return (
+                <>
+                    <table className="layout-table">
+                        <tbody>
+                            <tr>
+                                <td width='700px'>
+                                    <div className="video-frame">
+                                        <div className="video-responsive">
+                                            <iframe
+                                                width="600"
+                                                height="338"
+                                                src="https://www.youtube.com/embed/kZVZuW0VA4k"
+                                                title="Hello World"
+                                                frameBorder="0" 
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                allowFullScreen={true}>
+                                            </iframe>
                                         </div>
-                                    </td>
-                                    <td>
-                                        <div className="text-box">
-                                            <h3>Llana Ivanovich Grossman Crook</h3>
-                                            <span style={{ fontSize: '18px', fontWeight: 'bold', marginTop: '10px', marginBottom: '10px' }}>Bio:  </span>
-                                            <p>Llana formed an obsession taking apart watches and clocks at 7 years old. Blessed to have been born to a family with an unusually tech-inclined uncle, Llana was a proficient Commodore64 user by the time she was eight years old circa 1985, playing with programming in BASIC to make the device do interesting things. Her expanding relationship with technology had taken hold, resulting in her learning HTML the moment the internet became accessible, and moving on to the world of hacking. By the time Llana graduated high school in 1996, she was known in her family for her free computer and game console repair assistance, warranties be damned!</p>
-                                            <p>Many years later, Llana followed her love of technology from computers, to hacking, to information security and helping people. Professionally, she started with writing, then project managing, dabbling in marketing as she went, and eventually became a product manager. She cofounded two successful startups in the information security sphere. Now Llana is ready for her next big professional iteration, software architecture and engineering.</p>
-                                            <p>Find her at <a href="https://twitter.com/Llana">@Llana on X.com</a></p>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div className="text-box" style={{marginTop: '20px'}}>
-                            <h3>Project Links</h3>
-                            {/* Define iFrames for project links here */}
-                                <div>
-                                    <h3>Pacmen</h3>
-                                    <iframe src="/pacmen/index.html" title="Pacmen Project" width="100%" height="400"></iframe>
-                                </div>
-                                <div>
-                                    <h3>Bus Tracker</h3>
-                                    <iframe src="/bustracker/index.html" title="Bus Tracker Project" width="100%" height="400"></iframe>
-                                </div>
-                                <div>
-                                    <h3>Eyes</h3>
-                                    <iframe src="/eyes/index.html" title="Eyes Project" width="100%" height="400"></iframe>
-                                </div>
-                                {/* Assuming the Formik Form is an external link, keep as is */}
-                                <div>
-                                    <h3>Build a Formik Form</h3>
-                                    <p><a href="https://github.com/ProjectXIV/form_starter">Visit on GitHub</a></p>
-                                </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="text-box">
+                                        <h3>Llana Ivanovich Grossman Crook</h3>
+                                        {/* Biography and details here */}
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="text-box" style={{marginTop: '20px'}}>
+                        <h3>Project Links</h3>
+                        <div>
+                            <h3>Pacmen</h3>
+                            <iframe src="/pacmen/index.html" title="Pacmen Project" width="100%" height="400"></iframe>
                         </div>
-                    </>
-                );
-            case 'Connect':
-                return <p> <a href="https://twitter.com/Llana">@Llana on X.com</a></p>;
-            default:
-                return <p>Welcome to our website!</p>;
+                        <div>
+                            <h3>Bus Tracker</h3>
+                            <iframe src="/bustracker/index.html" title="Bus Tracker Project" width="100%" height="400"></iframe>
+                        </div>
+                        <div>
+                            <h3>Eyes</h3>
+                            <iframe src="/eyes/index.html" title="Eyes Project" width="100%" height="400"></iframe>
+                        </div>
+                        <div>
+                            <h3>Build a Formik Form</h3>
+                            <p><a href="https://github.com/ProjectXIV/form_starter">Visit on GitHub</a></p>
+                        </div>
+                    </div>
+                </>
+            );
+        }
+        // Handling for 'Connect' activeTab
+        else if (activeTab === 'Connect') {
+            return <p> <a href="https://twitter.com/Llana">@Llana on X.com</a></p>;
+        }
+        // Default case when no specific tab is active
+        else {
+            return <p>Welcome to our website!</p>;
         }
     }
-    
+
     render() {
         const { activeTab } = this.state;
 
