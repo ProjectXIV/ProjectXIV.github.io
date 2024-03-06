1942fa2c16172bf6bc80a33ca07637fba956afab
Project-XIV <elle@project-xiv.com>
2024-03-05 18:27:02 -0700

    because

--- xivapp/src/App.js
+++ xivapp/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import MainPage from './MainPage';
+import ContactPage from './ContactPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/contact" component={ContactPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
